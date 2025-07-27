efdc7c0fe9ca389821f293c7f7a7914b1a4b10c7
Obasi-sam <113913772+obasi-1@users.noreply.github.com>
2025-07-27 18:09:55 +0100

    Delete messaging_app/messaging_app/chats directory

--- messaging_app/messaging_app/chats/auth.py
+++
@@ -1,6 +0,0 @@
-# This file is intentionally left blank.
-#
-# It is created to follow the specified file structure.
-# Custom authentication logic, if needed in the future, can be placed here.
-# For this implementation, we are using the default JWTAuthentication class
-# from djangorestframework-simplejwt, which is configured in settings.py.
