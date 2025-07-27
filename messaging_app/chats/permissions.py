c122b2cf6f5f1369ca6090ff949b84d2e0f70d20
Obasi-sam <113913772+obasi-1@users.noreply.github.com>
2025-07-27 18:14:11 +0100

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,26 +1,37 @@
 from rest_framework import permissions
 
-class IsParticipantInConversation(permissions.BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     """
     Custom permission to only allow participants of a conversation to access it.
-    This permission checks if the requesting user is part of the conversation.
+    
+    This permission ensures that:
+    1. The user is authenticated.
+    2. The user is a participant in the conversation object being accessed.
     """
+
+    def has_permission(self, request, view):
+        """
+        Global permission check.
+        
+        This ensures that the user is authenticated before any other checks are made.
+        """
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         """
-        Check if the user is a participant in the conversation object.
+        Object-level permission check.
         
-        This method is called by DRF for detail views (GET, PUT, DELETE on /conversations/{id}/).
-        The `obj` here is an instance of the `Conversation` model.
+        This method is called for detail views (GET, PUT, DELETE) and checks if
+        the user is a participant of the specific `obj` instance.
+        
+        - If `obj` is a Conversation, it checks `obj.participants`.
+        - If `obj` is a Message, it checks the participants of `obj.conversation`.
         """
-        # We assume the `Conversation` model has a ManyToManyField named 'participants'
-        # that links to the User model.
-        # The permission is granted if the user making the request is in the set of participants.
+        # Check for Conversation objects. Assumes a 'participants' ManyToManyField.
         if hasattr(obj, 'participants'):
             return request.user in obj.participants.all()
         
-        # If the object is a Message, we check the conversation it belongs to.
-        # This assumes your Message model has a ForeignKey to Conversation named 'conversation'.
+        # Check for Message objects. Assumes a 'conversation' ForeignKey.
         if hasattr(obj, 'conversation'):
             return request.user in obj.conversation.participants.all()
             
