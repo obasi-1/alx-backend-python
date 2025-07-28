5c87fb81c89951f2278cf680dbe38cbff637e66a
Obasi-sam <113913772+obasi-1@users.noreply.github.com>
2025-07-28 05:31:14 +0100

    Update settings.py

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -1,105 +1,41 @@
-# Django-Middleware-0x03/alx-backend-python/settings.py
+# Django-Middleware-0x03/settings.py
 
-# This is a sample settings.py file.
-# You might have other settings here like BASE_DIR, SECRET_KEY, DEBUG, ALLOWED_HOSTS, etc.
+# This is a placeholder for your full settings.py file.
+# You would typically have many other settings here, such as:
+# DEBUG = True
+# ALLOWED_HOSTS = []
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'chats', # Make sure your 'chats' app is listed here
+# ]
+# DATABASES = { ... }
+# AUTH_PASSWORD_VALIDATORS = [ ... ]
+# LANGUAGE_CODE = 'en-us'
+# TIME_ZONE = 'UTC'
+# USE_I18N = True
+# USE_TZ = True
+# STATIC_URL = 'static/'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Add your 'chats' app here if it's not already present
-    'chats',
-]
-
+# --- MIDDLEWARE Configuration ---
+# This list defines the order in which middleware components are applied to requests.
+# Your custom middleware should be placed strategically.
+# It typically comes after AuthenticationMiddleware if it relies on user authentication.
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware', # Ensure this is before custom middlewares that use request.user
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # --- START HIGHLIGHT ---
-    # Add your custom middlewares here in the desired order
-    'chats.middleware.RequestLoggingMiddleware',
-    'chats.middleware.RestrictAccessByTimeMiddleware',
-    'chats.middleware.OffensiveLanguageMiddleware',
-    'chats.middleware.RolePermissionMiddleware', # <--- THIS IS THE LINE I ADDED/HIGHLIGHTED
-    # --- END HIGHLIGHT ---
+    'chats.middleware.RolepermissionMiddleware',
 ]
 
-ROOT_URLCONF = 'alx_backend_python.urls' # Adjust if your project name is different
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'alx_backend_python.wsgi.application' # Adjust if your project name is different
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# ... (any other settings you might have)
+# ... other settings would continue here ...
