b15df2f40d9007b5648e1c588b40df291d46668d
Obasi-sam <113913772+obasi-1@users.noreply.github.com>
2025-08-03 17:49:02 +0100

    Update managers.py

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,1 +1,13 @@
+# messaging/managers.py
+from django.db import models
 
+class UnreadMessagesManager(models.Manager):
+    """
+    Custom manager to filter for unread messages for a specific user.
+    """
+    def for_user(self, user):
+        """
+        Returns a queryset of unread messages for the specified user.
+        """
+        return self.filter(receiver=user, is_read=False).only('sender', 'content', 'timestamp')
+      
