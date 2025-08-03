d55f832ffc59c79d10e83092bb4a48bea4a04e56
Obasi-sam <113913772+obasi-1@users.noreply.github.com>
2025-08-03 16:54:55 +0100

    Create signals.py

---
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -0,0 +1,14 @@
+# messaging/signals.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Message, Notification
+
+@receiver(post_save, sender=Message)
+def create_notification(sender, instance, created, **kwargs):
+    """
+    Signal handler to automatically create a notification
+    when a new Message instance is saved.
+    """
+    if created:
+        # Create a notification for the message's receiver
+        Notification.objects.create(user=instance.receiver, message=instance)
